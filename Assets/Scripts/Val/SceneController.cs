3d18807d145a7ab0823ae1143296a6bb32c3f1c7
DrMonus <marioruizhtn@gmail.com>
2026-08-25 12:04:16 -0600

    Analitycs V16
    Need to be review, but semi funtional

--- Assets/Scripts/Val/SceneController.cs
+++ Assets/Scripts/Val/SceneController.cs
@@ -6,11 +6,13 @@ public class SceneController : MonoBehaviour
     public void LoadGameplayScene()
     {
         SDKAnalytics.Instance.TrackEvent("start_button_click");
+        GameplayAnalytics.Instance?.GameRestarted();
         SceneManager.LoadScene("02_GamePlay");
     }
 
     public void LoadMainMenuScene()
     {
+        GameplayAnalytics.Instance?.GameQuitEarly();
         SceneManager.LoadScene("01_MainMenu");
     }
 }
